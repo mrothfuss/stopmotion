3e56d8166ce2845a2667ca1b2f2423d5cd5ce20d
Mike Rothfuss <git@mikerothfuss.com>
2025-11-28 01:44:56 -0700

    improved runtime/snapshot implementation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import RPi.GPIO as GPIO
 import datetime
 
 import os
+import time
+import glob
 import sys
 import shutil
 import subprocess
@@ -25,28 +27,67 @@ class STATE(Enum):
 program_state = STATE.PENDING
 live_process = False
 
-def capture_frame():
+def find_vlc_capture():
+	snapshot_dir = os.path.expanduser("~")
+	prefix = "vlcsnap-"
+	ext = ".png"
+
+	pattern = os.path.join(snapshot_dir, f"{prefix}*{ext}")
+	i=1
+	while i<5:
+		files = glob.glob(pattern)
+
+		if files:
+			latest_file = max(files, key=os.path.getmtime)
+			return latest_file
+		time.sleep(1)
+		i = i + 1
+	return ""
+
+def update_overlay():
 	global project_frame
 
+	last_img="%s/frame_%05d.png" % (project_dir, project_frame)
+	overlay_img="%s/overlay.png" % (project_dir)
+	#subprocess.run(["magick", last_img, "-gravity", "Center" , "-crop", "1440x806+0+0", "-rotate", "180", "+repage", overlay_img])
+	subprocess.run(["magick", last_img, "-gravity", "Center" , "-crop", "1440x806+0+0", "+repage", overlay_img])
+	print("Created new overlay from %s" % last_img)
+
+def capture_frame():
+	global live_process
+	global project_frame
+
+	if not live_process:
+		return
+
+	live_process.stdin.write(b"snapshot\n")
+	live_process.stdin.flush()
+	last_img = find_vlc_capture()
+
+	if last_img == "":
+		return
+
 	project_frame = project_frame + 1
-	last_img="%s/last_001.png" % project_dir
-	subprocess.run(["ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-frames:v", "1", "%s/last_%%03d.png" % project_dir])
 	new_img="%s/frame_%05d.png" % (project_dir, project_frame)
 	print("MOVE %s to %s" % (last_img, new_img))
 	shutil.move(last_img, new_img)
+	update_overlay()
+	live_process.stdin.write(b"next\n")
+	live_process.stdin.flush()
 
 def start_live_stream():
 	global project_frame
 	global live_process
 
 	print("START STREAM")
-	last_img="%s/frame_%05d.png" % (project_dir, project_frame)
+	update_overlay()
 	overlay_img="%s/overlay.png" % (project_dir)
-	subprocess.run(["magick", last_img, "-gravity", "Center" , "-crop", "1440x806+0+0", "-rotate", "180", "+repage", overlay_img])
-	live_process = subprocess.Popen(["nvlc", "--sub-filter", "logo", "--logo-file", overlay_img, \
-		"--logo-opacity", "50", "--logo-x", "80", "--logo-y", "45", "v4l2:///dev/video0", \
+	live_process = subprocess.Popen(["cvlc", "--extraintf", "rc", "--sub-filter", "logo", "--logo-file", overlay_img, \
+		"--logo-opacity", "127", "--logo-position", "0", "v4l2:///dev/video0", "v4l2:///dev/video0", \
 		"--video-filter=transform", "--transform-type=180"], \
-		stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.PIPE)
+	#cvlc --extraintf rc --sub-filter logo --logo-file overlay.png --logo-opacity 50 --logo-x 80 --logo-y 45 v4l2:///dev/video0
+	# + snapshot
 
 	# nvlc --sub-filter logo --logo-file overlay.png --logo-opacity 50 --logo-x 80 --logo-y 45 v4l2:///dev/video0 --video-filter=transform --transform-type=180
 
@@ -59,6 +100,10 @@ def stop_live_stream():
 		live_process.kill()
 		live_process = False
 
+def restart_live_stream():
+	stop_live_stream()
+	start_live_stream()
+
 def change_state(next_state):
 	global program_state
 
@@ -68,11 +113,8 @@ def change_state(next_state):
 			start_live_stream()
 	if(program_state == STATE.LIVE):
 		if(next_state == STATE.CAPTURE):
-			stop_live_stream()
-			program_state = STATE.CAPTURE
 			capture_frame()
-			program_state = STATE.LIVE
-			start_live_stream()
+			#restart_live_stream()
 		if(next_state == STATE.PENDING):
 			stop_live_stream()
 			program_state = STATE.PENDING
